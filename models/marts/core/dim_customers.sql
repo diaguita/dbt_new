de38a7b7dfd91520030bdea0868fedbc1e713087
marcia_hereida@hotmail.com <marcia_hereida@hotmail.com>
2022-04-11 12:13:08 +0000

    dim_customers with 2 stgs

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,12 +1,8 @@
-{{ config(
-    materialized="table"
-)}}
-
-with customers as(
-    select * from {{ref('stg_customers')}}
+with customers as (
+    select * from {{ ref('stg_customers') }}
 ),
 orders as (
-    select *from {{ref('stg_orders')}}
+    select * from {{ ref('stg_orders') }}
 ),
 
 customer_orders as (
